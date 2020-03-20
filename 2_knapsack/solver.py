5b8e76405019fdf0964a071f9ad3e5331f467afc
TIF1 <duong.lequy@cyberlogitec.com>
2020-03-20 15:47:57 +0900

    update breadth first branch and bound & best BB

--- 2_knapsack/solver.py
+++ 2_knapsack/solver.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
+import numpy as np
 Item = namedtuple("Item", ['index', 'value', 'weight'])
+Node = namedtuple("Node", ['level', 'value', 'weight', 'idxTaken'])
 
 def greedy (capacity, items):
     value = 0
@@ -41,8 +43,7 @@ def dp (capacity, items):
         return []
     
     # Initialize the matrix contains optimal value gien (capacity, #items) 
-    #mat = [[0] * (l+1)] * (capacity+1)
-    mat = [[0] * (l+1) for i in range(capacity+1) ]
+    mat = np.zeros ((capacity+1, l+1), dtype=np.uint32)
     
     # Fill the matrix
     for idx in range (1, l+1):
@@ -51,9 +52,9 @@ def dp (capacity, items):
                 mat[k][idx] = mat[k][idx-1] 
                 continue
             else:
-                mat[k][idx] = max (mat[k][idx-1], items[idx-1].value + mat[k-items[idx-1].weight][idx-1])
+                mat[k][idx] = max (mat[k][idx-1], items[idx-1].value + \
+                        mat[k-items[idx-1].weight][idx-1])
     
-    #import pdb; pdb.set_trace()
     value = mat[capacity][l]
     taken = [0]*l
     
@@ -62,7 +63,6 @@ def dp (capacity, items):
     k = capacity
     for idx in range (l, 0, -1):
         while k >= 0:
-            #import pdb; pdb.set_trace()
             if check == 0:
                 check = mat[k][idx]
             if check == mat[k][idx-1]:
@@ -76,6 +76,270 @@ def dp (capacity, items):
                 
     
     return (value, taken)
+##### first try solution for breadth first branch and bound
+def getBound(node, capacity, items):
+    l = len (items)
+    temp = 0 
+
+    if node.weight > capacity:
+        return 0
+
+    # initialize
+    level = node.level
+    totWeight = node.weight
+    bound = node.value 
+
+    j = level + 1
+    if j < l:
+        temp = totWeight + items[j].weight
+    else: 
+        return bound
+
+    while (j < l and temp < capacity):
+        bound += items[j].value
+        totWeight = temp
+
+        j += 1
+        if j < l:
+            temp = totWeight + items[j].weight
+
+    if j < l:
+        bound += items[j].value * (capacity - totWeight)*1.0 / items[j].weight
+    return bound
+
+def breadth_first_branch_bound (capacity, items):
+
+    import queue
+
+    l = len (items)
+    # sort the item list by value density first
+    items.sort (key=lambda s: (-s.value/s.weight, -s.value))
+
+    #################################################################
+    v = Node (-1, 0, 0, [])
+
+    maxProfit = 0
+    bestTaken = []
+
+    q = queue.Queue ()
+    q.put (v)
+
+    while not q.empty():
+        u = q.get()
+
+        if u.level + 1 > l-1:
+            break
+
+        # If take the item -> 
+        level = u.level + 1
+        weight = u.weight + items[level].weight
+        value = u.value + items[level].value
+        idxTaken = u.idxTaken + [1]
+        v = Node (level, value, weight, idxTaken)
+
+        boundV = getBound (v, capacity, items)
+
+        if v.weight <= capacity and boundV > maxProfit:
+            q.put (v)
+
+        if v.weight <= capacity and v.value >= maxProfit:
+            maxProfit = v.value
+            bestTaken = v.idxTaken
+
+        #import pdb; pdb.set_trace()
+
+        # If don't take the item -> 
+        level = u.level + 1
+        weight = u.weight 
+        value = u.value 
+        idxTaken = u.idxTaken + [0]
+        v = Node (level, value, weight, idxTaken)
+
+        boundV = getBound (v, capacity, items)
+
+        if boundV > maxProfit:
+            q.put (v)
+
+
+    #import pdb; pdb.set_trace()
+    taken = [0] * l
+    for i, chosen in enumerate (bestTaken):
+        taken[items[i].index] = chosen
+
+
+    return (maxProfit, taken)
+########################################################
+
+##### optimal solution for breadth first branch and bound
+# - Important to have a good structure and define the meaning of Node \
+#        (firstly drawing the decision tree maybe the good approach)
+# - Important to implement getBound() function based on the Node structure
+Node2 = namedtuple ("Node2", "level value weight taken")
+def getBound2 (node, capacity, items):
+    l = len (items)
+
+    totWeight = node.weight
+    bound = node.value
+
+    j = node.level + 1
+    while j < l:
+        totWeight += items[j].weight
+        if (totWeight <= capacity):
+            bound += items[j].value
+            j += 1
+        else:
+            totWeight -= items[j].weight
+            break
+
+    if totWeight < capacity:
+        bound += (capacity - totWeight) * 1.0 / items[j-1].weight * items[j-1].value
+
+    return bound
+
+def bfs_branch_bound (capacity, items):
+    import queue
+
+    maxProfit = 0
+    bestTaken = []
+    root = Node2 (-1, 0, 0, [])
+
+    l = len (items)
+
+    q = queue.Queue()
+    #q = queue.LifoQueue()
+
+    q.put (root)
+
+    items.sort (key=lambda s: (-s.value/s.weight, -s.value))
+
+    while not q.empty():
+        u = q.get()
+
+        # Take the item
+        level = u.level + 1
+        if level > l-1:
+            break
+
+        value = u.value + items[level].value
+        weight = u.weight + items[level].weight
+        taken = u.taken + [1]
+        
+        v = Node2 (level, value, weight, taken)
+
+        boundV = getBound2 (v, capacity, items)
+
+        if v.weight <= capacity:
+            if boundV > maxProfit:
+                q.put (v)
+            if v.value >= maxProfit:
+                maxProfit = v.value
+                bestTaken = v.taken
+        print ("Take the item: {}, taken: {}".format (v, taken))
+        
+        # Don't take the item
+        level = u.level + 1
+        value = u.value
+        weight = u.weight
+        taken = u.taken + [0]
+
+        v = Node2 (level, value, weight, taken)
+
+        boundV = getBound2 (v, capacity, items)
+
+        if boundV >= maxProfit:
+            q.put (v)
+
+        print ("Don't take the item: {}, taken: {}".format (v, taken))
+
+    taken = [0] * l
+    for i, t in enumerate (bestTaken):
+        taken[items[i].index] = t
+
+    return maxProfit, taken
+
+
+# Depth first search branch and bound
+import heapq
+class PriorityQueue ():
+    def __init__ (self):
+        self.queue = []
+
+    def push (self, item, priority):
+        heapq.heappush (self.queue, (-priority, item)) # add a tuple to the queue, \
+                # with the descending order
+
+    def pop (self):
+        return heapq.heappop (self.queue)[-1]
+
+    def empty(self):
+        return (len (self.queue) == 0)
+
+# Always consider the node with largest bound
+# Same with bfs but use a priority queue to store node
+def best_first_branch_bound (capacity, items):
+    import queue
+
+    maxProfit = 0
+    bestTaken = []
+    root = Node2 (-1, 0, 0, [])
+
+    l = len (items)
+
+    q = PriorityQueue()
+
+    #import pdb; pdb.set_trace()
+    q.push (root, 0)
+
+    items.sort (key=lambda s: (-s.value/s.weight, -s.value))
+
+    while not q.empty():
+        u = q.pop()
+
+        # Take the item
+        level = u.level + 1
+        if level > l-1:
+            break
+
+        value = u.value + items[level].value
+        weight = u.weight + items[level].weight
+        taken = u.taken + [1]
+        
+        v = Node2 (level, value, weight, taken)
+
+        boundV = getBound (v, capacity, items)
+
+        if v.weight <= capacity:
+            if boundV > maxProfit:
+                q.push (v, boundV)
+            if v.value >= maxProfit:
+                maxProfit = v.value
+                bestTaken = v.taken
+        #print ("Take the item: {}, bound: {}, maxProfit: {}".\
+        #        format (v, boundV, maxProfit))
+        
+        # Don't take the item
+        level = u.level + 1
+        value = u.value
+        weight = u.weight
+        taken = u.taken + [0]
+
+        v = Node2 (level, value, weight, taken)
+
+        boundV = getBound (v, capacity, items)
+
+        if boundV >= maxProfit:
+            q.push (v, boundV)
+
+        #print ("Don't take the item: {}, bound: {}, maxProfit: {}".\
+        #        format (v, boundV, maxProfit))
+
+    taken = [0] * l
+    for i, t in enumerate (bestTaken):
+        taken[items[i].index] = t
+
+    return maxProfit, taken
+########################################################
+
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -110,10 +374,16 @@ def solve_it(input_data):
     #value, taken = greedy (capacity, items)
     
     # dp solution
-    value, taken = dp (capacity, items)
+    #value, taken = dp (capacity, items)
+
+    # branch and bound solution
+    #value, taken = breadth_first_branch_bound (capacity, items)
+    #value, taken = bfs_branch_bound (capacity, items)
+    value, taken = best_first_branch_bound (capacity, items)
     
     # prepare the solution in the specified output format
-    output_data = str(value) + ' ' + str(0) + '\n'
+    #output_data = str(value) + ' ' + str(0) + '\n'
+    output_data = str(value) + ' ' + str(1) + '\n'
     output_data += ' '.join(map(str, taken))
     return output_data
 
@@ -129,7 +399,7 @@ def solve_it(input_data):
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
 """
 if __name__ == '__main__':
-    file_location = "./data/ks_lecture_dp_2"
+    file_location = "./data/ks_4_0"
     
     with open(file_location, 'r') as input_data_file:
         input_data = input_data_file.read()
